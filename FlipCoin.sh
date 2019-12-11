96b5d38a1fdd3728a0d7239253ec88313eebf833
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-11 09:31:37 +0530

    Added : Calculated singlet and store result in dict

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -4,10 +4,28 @@ echo "Welcome To The Flip Coin"
 
 flipCoinResult=$((RANDOM%2))
 
-if [ $flipCoinResult -eq 1 ]
-then
-	echo "Head"
+read -p "Enter How Many Times U Want To Flip Coin" num
+head=0
+tail=0
 
-else
-	echo "Tail"
-fi
+declare -A flipDict
+
+for (( i=1;i<=$num;i++))
+do
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq 1 ]
+	then
+	head=$(($head+1))
+	flipDict["flipCoin$i"]="head"
+	else
+	tail=$(($tail+1))
+	flipDict["flipCoin$i"]="tail"
+	fi
+done
+
+echo "head : "$head
+echo "Tail : "$tail
+for(( i=1; i<=${#flipDict[@]}; i++ ))
+do
+	echo "FlipCoin $i     ${flipDict[flipCoin$i]}"
+done
